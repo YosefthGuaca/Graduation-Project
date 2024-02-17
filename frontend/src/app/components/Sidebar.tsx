4fad9cadc6f5f3ba90f0a24040fee764fdd5b3f9
obrkn <k33mdukb9n@yahoo.co.jp>
2024-02-17 14:38:57 -0800

    array file structure

---
+++ frontend/src/app/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const sidebar = (props: Props) => {
+  return <div>sidebar</div>;
+};
+
+export default sidebar;
---
+++ frontend/src/app/q/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>This is Published Portfolio Top Page</div>;
+};
+
+export default page;
