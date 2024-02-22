211de1f81d31a66b72da2310de789712e485e2f2
orange-na <otuy1125abman@gmail.com>
2024-02-22 15:25:12 -0800

    [Admin] Modify typescript settings similar to the backend

---
+++ admin/test/server.test.ts
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+import app from "../src/server";
+
+const request = supertest(app);
+
+describe("GET /", () => {
+  it('responds with "Hello world!"', async () => {
+    const response = await request.get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello world!");
+  });
+});
