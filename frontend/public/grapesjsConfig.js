a1843e261d2822f5f02710cd427f7072410a111d
obrkn <k33mdukb9n@yahoo.co.jp>
2024-02-28 22:28:36 -0800

    added grapesjs module

--- frontend/public/grapesjsConfig.js
+++ frontend/public/grapesjsConfig.js
@@ -1,4 +1,24 @@
+const projectID = 1;
+const projectEndpoint = `http://localhost:4000/projects/${projectID}`;
 const editor = grapesjs.init({
   container: '#gjs',
   plugins: ['grapesjs-preset-webpage'],
+  storageManager: {
+    type: 'remote',
+    stepsBeforeSave: 3,
+    options: {
+      remote: {
+        urlLoad: projectEndpoint,
+        urlStore: projectEndpoint,
+        // The `remote` storage uses the POST method when stores data but
+        // the json-server API requires PATCH.
+        fetchOptions: (opts) => (opts.method === 'POST' ? { method: 'PATCH' } : {}),
+        // As the API stores projects in this format `{id: 1, data: projectData }`,
+        // we have to properly update the body before the store and extract the
+        // project data from the response result.
+        onStore: (data) => ({ id: projectID, data }),
+        onLoad: (result) => result.data,
+      },
+    },
+  },
 });
