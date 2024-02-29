90e2ea962661240b14eb124014effd635c6988bf
obrkn <k33mdukb9n@yahoo.co.jp>
2024-02-28 23:44:53 -0800

    move css file to public

--- frontend/src/app/home/q/[slug]/edit/page.tsx
+++ frontend/src/app/home/q/[slug]/edit/page.tsx
@@ -1,28 +1,7 @@
 type Props = {};
 
 const Page = (props: Props) => {
-  return (
-    <>
-      <div className="panel__top">
-        <div className="panel__basic-actions"></div>
-        <div className="panel__devices"></div>
-        <div className="panel__switcher"></div>
-      </div>
-      <div className="editor-row">
-        <div className="editor-canvas">
-          <div id="gjs">
-            <h1>Hello World Component!</h1>
-          </div>
-        </div>
-        <div className="panel__right">
-          <div className="layers-container"></div>
-          <div className="styles-container"></div>
-          <div className="traits-container"></div>
-        </div>
-      </div>
-      <div id="blocks"></div>
-    </>
-  );
+  return <div id="gjs" />;
 };
 
 export default Page;
