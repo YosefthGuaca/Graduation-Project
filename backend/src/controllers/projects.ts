317b4417710768f1bb96a07c98ee17fd4611e5fe
orange-na <otuy1125abman@gmail.com>
2024-03-04 13:34:11 -0800

    [GrapesJS] Save GrapesJS JSON data in the Database

---
+++ backend/src/controllers/projects.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const getPage = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  const projects = await prisma.page
+    .findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      select: {
+        content: true,
+      },
+    })
+    .catch((error: Error) => {
+      return res.status(500).json(error);
+    });
+
+  return res.status(200).json(projects);
+};
+
+const createPage = async (req: express.Request, res: express.Response) => {
+  const page = await prisma.page
+    .create({
+      data: {
+        slug: "slug",
+        content: { page: "content" },
+        name: "name",
+        versionId: 1,
+      },
+    })
+    .catch((error: Error) => {
+      return res.status(500).json(error);
+    });
+
+  return res.status(201).json(page);
+};
+
+const updatePage = async (req: express.Request, res: express.Response) => {
+  const content = req.body;
+  const page = await prisma.page
+    .update({
+      where: {
+        id: 1,
+      },
+      data: {
+        slug: "slug",
+        content,
+        name: "name",
+        versionId: 1,
+      },
+    })
+    .catch((error: Error) => {
+      return res.status(500).json(error);
+    });
+
+  return res.status(200).json(page);
+};
+
+export { getPage, createPage, updatePage };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import corsRoutes from "./routes/corsRoutes";
+import projectsRoutes from "./routes/projects";
+import cors from "cors";
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+  credentials: true,
+  methods: "GET, POST, DELETE, PATCH, PUT",
+  allowedHeaders: ["Content-Type"],
+};
 
 dotenv.config();
 
 const app: Express = express();
-app.use("/api", corsRoutes);
-const port = 4000;
+app.use(cors(corsOptions));
 app.use(express.json());
+
+const port = 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello World!" });
@@ -16,5 +26,8 @@ app.get("/", (req: Request, res: Response) => {
 const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+app.use("/api", corsRoutes);
+app.use("/projects", projectsRoutes);
 
 export default server;
