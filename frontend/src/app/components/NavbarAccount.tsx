616e07f8bae6896c951d6f41387e6860608e8a3c
geoayalamung <paao.ayala@gmail.com>
2024-04-19 10:51:24 -0700

    logo

--- frontend/src/app/components/NavbarAccount.tsx
+++ frontend/src/app/components/NavbarAccount.tsx
@@ -18,8 +18,8 @@ const NavbarAccount: React.FC = () => {
         <div className="relative flex items-center justify-between" style={{ height: '120px' }}> 
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
-              {/* <img className="block lg:hidden h-8 w-auto" src="/UxShowGoDark.png" alt="Logo" />
-              <img className="hidden lg:block h-8 w-auto" src="/UxShowGoDark.png" alt="Logo" /> */}
+              <img className="block lg:hidden h-8 w-auto" src="/UxShowGoDark.png" alt="Logo" />
+              <img className="hidden lg:block h-8 w-auto" src="/UxShowGoDark.png" alt="Logo" />
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-6">  
